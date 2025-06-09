e401f84aed0a2e67efc15bcf1a1bd24a1384fadb
Carlos Lovera <calovera@bu.edu>
2025-06-09 18:28:43 -0400

    Add solution: TwoSum

---
+++ Algorithms/TwoSum.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1659114420 lang=javascript
+ *
+ * TwoSum
+ * 
+ * Difficulty: Easy
+ * Category: Algorithms
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        map.set(nums[i], i);
+    }
+
+    return []; // in case no solution found
+};
