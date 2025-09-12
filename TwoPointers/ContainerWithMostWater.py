b8305024d48f5190f2e8a4c49d5a7e35afa0b2be
Carlos Lovera <calovera@bu.edu>
2025-09-12 00:06:07 -0400

    Add solution: ContainerWithMostWater

---
+++ TwoPointers/ContainerWithMostWater.py
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1767802056 lang=python3
+ *
+ * ContainerWithMostWater
+ * 
+ * Difficulty: Level
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        left,right = 0, len(height)-1
+        result = 0
+
+        while left < right:
+            area = min(height[left], height[right]) * (right - left)
+            result = max(result, area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return result
